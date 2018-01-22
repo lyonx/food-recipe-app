0f2af0836f6819ee305e05985b9c07f36d6e2549
Ben <ben.harloe@gmail.com>
2018-01-22 14:07:22 -0800

    models fixed

--- models/user.js
+++ models/user.js
@@ -1,22 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("user", {
+    var User = sequelize.define("User", {
 
       username: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING
     });
   
-    // User.associate = function(models) {
-    //   User.hasMany(models.Ingredient, {
-    //     onDelete: "cascade"
-    //   });
-    //   User.hasMany(models.Recipe, {
-    //     onDelete: "cascade"
-    //   });
-    //   User.hasMany(models.FavoriteRecipe, {
-    //     onDelete: "cascade"
-    //   });
-    // };
+    User.associate = function(models) {
+      User.hasMany(models.Ingredient, {
+        onDelete: "cascade"
+      });
+      User.hasMany(models.Recipe, {
+        onDelete: "cascade"
+      });
+      User.hasMany(models.FavoriteRecipe, {
+        onDelete: "cascade"
+      });
+    };
   
     return User;
   };
